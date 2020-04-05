276500fe67e52af1e2eb24141b7abdcb66b803d7
Nikita <skok2783@gmail.com>
2020-04-05 13:48:25 +0700

    add life cycle method

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -4,18 +4,15 @@ import * as axios from 'axios';
 import userPhoto from '../../img/user.png';
 
 class Users extends React.Component {
-   
-    constructor(props) {
-        super(props);
-        
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
     }
+
         render() {
             return ( 
-            <div>
             <div className={style.users}>
                {
                     this.props.users.map( u => <div key={u.id} className={style.users__shortlog} >
@@ -49,7 +46,6 @@ class Users extends React.Component {
     
                </div> )}
             </div>
-        </div>
         )
     }
 }
